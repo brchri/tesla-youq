d61bbc936e8506b6a375b0b6eb68736b8e3b3331
brchri <126272303+brchri@users.noreply.github.com>
2023-08-06 20:43:31 -0600

    update comments

--- internal/util/config.go
+++ internal/util/config.go
@@ -13,26 +13,29 @@ type (
 		Lng float64 `yaml:"lng"`
 	}
 
-	// defines which geofence change will trigger an event, e.g. "home" to "not_home"
+	// defines which teslamate defined geofence change will trigger an event, e.g. "home" to "not_home"
 	TeslamateGeofenceTrigger struct {
 		From string `yaml:"from"`
 		To   string `yaml:"to"`
 	}
 
+	// defines a center point and two radii (distances) to define open and close geofences
 	CircularGeofence struct {
 		Center        Point   `yaml:"center"`
-		CloseDistance float64 `yaml:"close_distance"`
-		OpenDistance  float64 `yaml:"open_distance"`
+		CloseDistance float64 `yaml:"close_distance"` // defines a radius from the center point; when vehicle moves from < distance to > distance, garage will close
+		OpenDistance  float64 `yaml:"open_distance"`  // defines a radius from the center point; when vehicle moves from > distance to < distance, garage will open
 	}
 
+	// defines triggers for open and close action for teslamate geofences
 	TeslamateGeofence struct {
-		Close TeslamateGeofenceTrigger `yaml:"close_trigger"`
-		Open  TeslamateGeofenceTrigger `yaml:"open_trigger"`
+		Close TeslamateGeofenceTrigger `yaml:"close_trigger"` // garage will close when vehicle moves from `from` to `to`
+		Open  TeslamateGeofenceTrigger `yaml:"open_trigger"`  // garage will open when vehicle moves from `from` to `to`
 	}
 
+	// contains 2 geofences, open and close, each of which are a list of lat/long points defining the polygon
 	PolygonGeofence struct {
-		Close []Point `yaml:"close"`
-		Open  []Point `yaml:"open"`
+		Close []Point `yaml:"close"` // list of points defining a polygon; when vehicle moves from inside this geofence to outside, garage will close
+		Open  []Point `yaml:"open"`  // list of points defining a polygon; when vehicle moves from outside this geofence to inside, garage will open
 	}
 
 	Car struct {
@@ -47,9 +50,9 @@ type (
 		InsidePolyCloseGeo bool        // indicates if car is currently inside the polygon_close_geofence
 	}
 
-	// defines a garage door with either a location and open/close radii, OR trigger open/close geofences
-	// {Location,CloseRadius,OpenRadius} set is mutually exclusive with {TriggerCloseGeofence,TriggerOpenGeofence}
-	// with preference for {TriggerCloseGeofence,TriggerOpenGeofence} set if both are defined
+	// defines a garage door with one unique geofence type: circular, teslamate, or polygon
+	// only one geofence type may be defined per garage door
+	// if more than one defined, priority will be polygon > circular > teslamate
 	GarageDoor struct {
 		CircularGeofence  CircularGeofence  `yaml:"circular_geofence"`
 		TeslamateGeofence TeslamateGeofence `yaml:"teslamate_geofence"`
@@ -82,12 +85,12 @@ var Config ConfigStruct
 
 const (
 	PolygonGeofenceType   = "PolygonGeofence"   // custom polygon geofence defined by multiple lat/long points
-	CircularGeofenceType  = "CircularGeofence"  // circle geofence with center point and radius
+	CircularGeofenceType  = "CircularGeofence"  // circular geofence with center point and radius
 	TeslamateGeofenceType = "TeslamateGeofence" // geofence defined in teslamate
 )
 
 // checks for valid geofence values for a garage door
-// preferred priority is polygon > circle > teslamate
+// preferred priority is polygon > circular > teslamate
 func (g GarageDoor) GetGeofenceType() string {
 	if len(g.PolygonGeofence.Open) > 0 && len(g.PolygonGeofence.Close) > 0 {
 		return PolygonGeofenceType
